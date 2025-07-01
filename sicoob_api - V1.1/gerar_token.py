9d1f4df472fcca0b9166264756e390e26907355d
Orion Graciano <87495924+oriongraciano@users.noreply.github.com>
2025-07-01 15:10:49 -0300

    ajustes

--- sicoob_api - V1.1/gerar_token.py
+++
@@ -1,6 +0,0 @@
-from services.auth import gerar_token
-
-if __name__ == "__main__":
-    token = gerar_token()
-else:
-    print("Erro ao gerar token.")
--- sicoob_api - V1.2/gerar_token.py
+++
@@ -1,8 +0,0 @@
-from services.auth import gerar_token
-
-if __name__ == "__main__":
-    token = gerar_token()
-    print("TOKEN GERADO COM SUCESSO:")
-    print(token)
-else:
-    print("Erro ao gerar token.")
